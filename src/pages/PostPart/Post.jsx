063b92aba84085a5e56f491b20a840612d90ae39
jibonrahaman <manvirjibon@gmail.com>
2023-12-06 23:36:14 +0600

    edit and delet icon design

--- src/pages/PostPart/Post.jsx
+++ src/pages/PostPart/Post.jsx
@@ -6,16 +6,16 @@ import { MdDelete } from "react-icons/md";
 function Post() {
   return (
     <section className=' post rounded-lg py-4 bg-[#242526]'>
-     <div>
+     <div className='group'>
         <Flex className=' relative text-white gap-x-3 items-center px-3'>
             <img src={user} alt={user} className=' w-12 h-12 rounded-full' />
             <div className='' >
              <p className=' text-xl'>Md Shahriar Fardous</p>
             <span className=' text-[#82858a] text-[12px]'>Time 2 pm </span>
             </div>
-            <Flex className=" gap-x-9 absolute right-4 text-3xl top-2">
-            <CiEdit />
-            <MdDelete />
+            <Flex className="  gap-x-9 absolute right-4 text-3xl  ">
+            <CiEdit className='group-hover:bg-black rounded-full w-12 h-12 p-2 duration-500' />
+            <MdDelete  className='group-hover:bg-black rounded-full w-12 h-12 p-2 duration-500'/>
             </Flex>
         </Flex>
      </div>
