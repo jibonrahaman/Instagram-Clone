404686776d8223298769e00929bf05b70023e888
jibonrahaman <manvirjibon@gmail.com>
2023-12-06 23:24:32 +0600

    edit and delet icon design

--- src/pages/PostPart/Post.jsx
+++ src/pages/PostPart/Post.jsx
@@ -7,13 +7,13 @@ function Post() {
   return (
     <section className=' post rounded-lg py-4 bg-[#242526]'>
      <div>
-        <Flex className=' text-white gap-x-3 items-center px-3'>
+        <Flex className=' relative text-white gap-x-3 items-center px-3'>
             <img src={user} alt={user} className=' w-12 h-12 rounded-full' />
             <div className='' >
              <p className=' text-xl'>Md Shahriar Fardous</p>
             <span className=' text-[#82858a] text-[12px]'>Time 2 pm </span>
             </div>
-            <Flex className="">
+            <Flex className=" gap-x-9 absolute right-4 text-3xl top-2">
             <CiEdit />
             <MdDelete />
             </Flex>
