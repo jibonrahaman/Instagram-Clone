b17ddb5701f99d1f1664b66493c673396649213e
jibonrahaman <manvirjibon@gmail.com>
2023-12-09 21:20:02 +0600

    moment js

--- src/pages/PostPart/Post.jsx
+++ src/pages/PostPart/Post.jsx
@@ -8,6 +8,7 @@ import fb from '../../assets/fb.jpg'
 import { getDatabase, ref, onValue } from "firebase/database";
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import moment from 'moment';
 function Post() {
   const [postShow, setpostShow] =useState([]);
   const db = getDatabase();
@@ -31,7 +32,9 @@ useEffect(()=>{
   onValue(postRef, (snapshot) => {
     let arr =[]
     snapshot.forEach((item)=>{
-     arr.push(item.val());
+    if(data.uid != item.val().postsendid){
+      arr.push(item.val());
+    }
     })
     setpostShow(arr)
   });
@@ -41,12 +44,16 @@ useEffect(()=>{
     {
   postShow.map((item,index)=>(
    <section key={index}>
- <div className=' post rounded-lg pt-4 group    text-white mt-4 bg-[#242526]'>
+ <div className=' post rounded-lg pt-4 group  my-8  text-white mt-4 bg-[#242526]'>
     <Flex className=' relative  gap-x-3 items-center px-3'>
             <img src={item.postsendPhoto} alt={item.postsendPhoto} className=' w-12 h-12 rounded-full' />
             <div className='' >
              <p className=' text-xl'>{item.postsendname}</p>
-            <span className=' text-[#82858a] text-[12px]'>Time 2 pm </span>
+            <span className=' text-[#82858a] text-[12px]'>
+              {
+                  moment(item.date, "YYYYMMDD hh:mm:ss a").fromNow()
+              } 
+              </span>
             </div>
             <Flex className="  gap-x-9 absolute right-4 text-3xl  ">
             <CiEdit className='group-hover:bg-black rounded-full w-12 h-12 p-2 duration-500' />
