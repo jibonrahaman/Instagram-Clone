ff0208263347f87d908a70cd2aef3f4075060d90
jibonrahaman <manvirjibon@gmail.com>
2023-12-16 23:52:27 +0600

    userList  read firebase  people page

--- src/pages/People/People.jsx
+++ src/pages/People/People.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Flex from '../../Components/Flex'
  import user from '../../assets/user.jpg'
+import { useSelector } from 'react-redux';
+import { getDatabase, onValue, ref } from 'firebase/database';
 function People() {
+  const data =useSelector(state => state.userLoginInfo.userInfo)
+  const db =getDatabase();
+  const [userList , setuserList] =useState([]);
+  useEffect(() => {
+    const userRef = ref(db, 'users/');
+    onValue(userRef, (snapshot) => {
+      let arr = []
+      snapshot.forEach(item => {
+        if (data.uid != item.key) {
+          arr.push({ ...item.val(), userid: item.key });
+        }
+      })
+      setuserList(arr)
+    });
+  }, [])
+
   return (
-    <section className='w-[80%] h-screen overflow-y-scroll'>
+    <section className='w-[80%] h-screen overflow-y-scroll bg-[#242526]'>
        <div className='w-[40%] -600 mt-28'>
         <h2 className='text-white ml-48 my-2'>Suggested</h2>
-       <Flex className=" justify-around mt-1">
-        <Flex className="text-white  gap-x-3">
-        <img src={user} alt={user}  className=' w-14 h-14 rounded-full'/>
-         <h2 className='mt-2 text-xl'>Jibon</h2>
-        </Flex>
-        <div>
-        <button className=' text-md px-4 py-1 mt-2 bg-[#0095f6] hover:bg-[#1877f2] text-white rounded-md '>Follow</button>
-        </div>
-        </Flex>
+      
        </div>
     </section>
   )
--- src/pages/Users/UserList.jsx
+++ src/pages/Users/UserList.jsx
@@ -1,45 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import Flex from '../../Components/Flex'
-import { getDatabase, onValue, ref } from 'firebase/database';
 import { useSelector } from 'react-redux';
-import user from '../../assets/user.jpg'
 import { Link } from 'react-router-dom';
 function UserList() {
   const data =useSelector(state => state.userLoginInfo.userInfo)
-  const db =getDatabase();
-  const [userList , setuserList] =useState([]);
-  useEffect(() => {
-    const userRef = ref(db, 'users/');
-    onValue(userRef, (snapshot) => {
-      let arr = []
-      snapshot.forEach(item => {
-        if (data.uid != item.key) {
-          arr.push({ ...item.val(), userid: item.key });
-        }
-      })
-      setuserList(arr)
-    });
-  }, [])
-  const handlePeople=()=>{
-    console.log("item");
-  }
-
-
-//   <div>
-//   {
-//     userList.map((item,index)=>(
-//      <div key={index}>
-//       <Flex className="mt-6 gap-x-1 justify-between">
-//  <Flex className="gap-x-2"> 
-//  <img src={item.userImgUrl} alt={item.userImgUrl} className='w-12 h-12 rounded-full' />
-//      <p className=' text-xl mt-[2px]  '>{item.userName}</p>
-//  </Flex>
-//  <p className='text-[#004b7b] mt-1 group-hover:text-white duration-300 text-[18px] group-hover:mt-[-10px] '>follow</p>
-//    </Flex>
-//      </div>
-//     ))
-//   }
-//  </div>
+ 
+ 
+  
   return (
    <section className='px-4 text-white group '>
 
@@ -55,7 +22,7 @@ function UserList() {
      <Flex className=" mt-8 ml-5 justify-between">      
      <p className=' text-[#a3a3a3]'>Suggested for you</p>  
     <Link to="/people">
-    <button onClick={handlePeople}>See all </button> </Link>
+    <button >See all </button> </Link>
      </Flex>
     </section>
   )
