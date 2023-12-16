49622b1427b015327f2baf10ec9b1f0b0b2acc0d
jibonrahaman <manvirjibon@gmail.com>
2023-12-16 23:59:47 +0600

    userList map

--- src/pages/People/People.jsx
+++ src/pages/People/People.jsx
@@ -24,21 +24,19 @@ function People() {
     <section className='w-[80%] h-screen overflow-y-scroll bg-[#242526]'>
        <div className='w-[40%] -600 mt-28'>
         <h2 className='text-white ml-48 my-2'>Suggested</h2>
-      {
+       {
         userList.map((item,index)=>(
-          <div key={index}>
-            <Flex className=" justify-around mt-1">
-          <Flex className="text-white  gap-x-3">
-          <img src={user} alt={user}  className=' w-14 h-14 rounded-full'/>
-           <h2 className='mt-2 text-xl'>Jibon</h2>
-          </Flex>
-          <div>
-          <button className=' text-md px-4 py-1 mt-2 bg-[#0095f6] hover:bg-[#1877f2] text-white rounded-md '>Follow</button>
-          </div>
-          </Flex>
-          </div>
+          <Flex key={index} className=" justify-around mt-5 ml-36" >
+        <Flex className="text-white  gap-x-3">
+        <img src={user} alt={user}  className=' w-14 h-14 rounded-full'/>
+         <h2 className='mt-2 text-xl'>Jibon</h2>
+        </Flex>
+        <div>
+        <button className=' text-md px-4 py-1 mt-2 bg-[#0095f6] hover:bg-[#1877f2] text-white rounded-md '>Follow</button>
+        </div>
+        </Flex>
         ))
-      }
+       }
        </div>
     </section>
   )
