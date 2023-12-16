e5f49f568c09272db2358d7c197c21571d103866
jibonrahaman <manvirjibon@gmail.com>
2023-12-16 23:46:44 +0600

    people page design

--- src/pages/People/People.jsx
+++ src/pages/People/People.jsx
@@ -7,7 +7,13 @@ function People() {
        <div className='w-[40%] -600 mt-28'>
         <h2 className='text-white ml-48 my-2'>Suggested</h2>
        <Flex className=" justify-around mt-1">
-         <img src={user} alt={user}  className=' w-14 h-14 rounded-full'/>
+        <Flex className="text-white  gap-x-3">
+        <img src={user} alt={user}  className=' w-14 h-14 rounded-full'/>
+         <h2 className='mt-2 text-xl'>Jibon</h2>
+        </Flex>
+        <div>
+        <button className=' text-md px-4 py-1 mt-2 bg-[#0095f6] hover:bg-[#1877f2] text-white rounded-md '>Follow</button>
+        </div>
         </Flex>
        </div>
     </section>
